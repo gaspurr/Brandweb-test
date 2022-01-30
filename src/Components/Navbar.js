f23f7958142ca7c42b8e76281ef357cc5d36adcc
gaspurr <luik.gaspar@gmail.com>
2022-01-30 21:58:52 +0200

    Navbar searchbar placement

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { InputForm, Form } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 import "./Navbar.css"
 
@@ -8,13 +9,16 @@ function Navbar() {
             <nav className="navbar">
                 <div className="navbar-container">
                     <NavLink to="/" className="nav-logo">
-                        Farmy
+                        Brandweb
                     </NavLink>
                     <ul className="nav-menu">
                         <li className="nav-item">
-                            <NavLink to="/" className="nav-links">
-                                Search
-                            </NavLink>
+                            <Form.Label htmlFor="Searchbar" />
+                            <Form.Control
+                                type="text"
+                                id="search-query"
+                                placeholder="Look up a game"
+                            />
                         </li>
                     </ul>
                 </div>
